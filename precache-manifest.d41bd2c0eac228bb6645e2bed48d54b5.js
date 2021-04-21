4c5914e7acbf1c17ece888ee7423f7e5d11d746b
Tushar1401 <tushararora1401@gmail.com>
2021-04-21 13:10:03 +0530

    Updates

---
+++ precache-manifest.d41bd2c0eac228bb6645e2bed48d54b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8c0d1a57cdd18d5ac756d12358b27f",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "2e2415282f721785ac63",
+    "url": "/netflix-clone/static/css/main.435dafa1.chunk.css"
+  },
+  {
+    "revision": "5d980e23eb4bce59b216",
+    "url": "/netflix-clone/static/js/2.95dc9622.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.95dc9622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2415282f721785ac63",
+    "url": "/netflix-clone/static/js/main.9eab4c1a.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
